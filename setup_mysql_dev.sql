2e19330d38357088861450f1cd18709ae740924d
root <d.ndungu@alustudent.com>
2023-10-05 07:39:55 -0700

    MYSQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,7 @@
+-- Creata a hbnb database if it doesnt exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Creates a new user in localhost
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grants hbnb_dev privileges to database
 GRANT ALL PRIVILEGES on hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
