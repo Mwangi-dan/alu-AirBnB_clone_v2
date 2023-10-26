43e32ed11488355a74e4989d540dabd41d8cce31
mwangi_dan <d.ndngu@alustudent.com>
2023-10-26 16:41:07 +0000

    number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -49,7 +49,7 @@ def number(n):
     prints n if n is a number
     """
     n = str(n)
-    return "{} is a number"
+    return "{} is a number".format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
