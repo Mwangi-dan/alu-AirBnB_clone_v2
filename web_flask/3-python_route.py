0047f8a7a14ac661d0d102a43d9f5eb525915e33
mwangi_dan <d.ndngu@alustudent.com>
2023-10-26 16:15:08 +0000

    python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Start Flask application and display HBNB """
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """
+    Returns `HBNB`
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """
+    returns other text
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """
+    returns text with text witten after c
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/(<text>)', strict_slashes=False)
+def python_text():
+    """
+    python text
+    """
+    if text == None:
+        return "Python is cool"
+    else:
+        text = text.replace('_', ' ')
+        return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
