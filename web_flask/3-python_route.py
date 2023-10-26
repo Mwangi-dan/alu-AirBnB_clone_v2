693af00394ee1cd46cbb4a94dd710baa019fb750
mwangi_dan <d.ndngu@alustudent.com>
2023-10-26 16:33:24 +0000

    python

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -34,8 +34,8 @@ def c_is_fun(text):
 
 
 @app.route('/python/', strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
-def python_text(text="is cool"):
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
     """
     python text
     """
