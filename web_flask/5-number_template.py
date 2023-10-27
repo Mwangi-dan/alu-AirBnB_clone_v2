5772858e0dee22395133bb6120bb10b16b5a6297
mwangi_dan <d.ndngu@alustudent.com>
2023-10-27 12:07:16 +0000

    number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""" Start Flask application and display HBNB """
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """
+    Returns `HBNB`
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """
+    returns other text
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """
+    returns text with text witten after c
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """
+    python text
+    """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    prints n if n is a number
+    """
+    n = str(n)
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Number template in html file only if number is an integer
+    """
+    return render_template("5-number.html", n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
